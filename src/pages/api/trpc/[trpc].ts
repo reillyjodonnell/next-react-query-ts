1a5b57d6117e67ae38d3fe036970a515a67314de
Reilly O'Donnell <odonnell.reilly.j@gmail.com>
2022-10-04 23:56:51 -0400

    Implemented basic trpc router and functions

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,37 @@
+import { initTRPC } from '@trpc/server';
+import * as trpcNext from '@trpc/server/adapters/next';
+import { z } from 'zod';
+import { prisma } from '../../../server/prisma';
+export const t = initTRPC.create();
+
+export const appRouter = t.router({
+  hello: t.procedure
+    .input(
+      z
+        .object({
+          text: z.string().nullish(),
+        })
+        .nullish()
+    )
+    .query(({ input }) => {
+      return {
+        greeting: `hello ${input?.text ?? 'world'}`,
+      };
+    }),
+
+  items: t.procedure
+    .input(z.object({ text: z.string().nullish() }).nullish())
+    .query(async ({ input }) => {
+      const data = await prisma.product.findMany();
+      return data;
+    }),
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
+
+// export API handler
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  createContext: () => ({}),
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,76 @@
-import { GetServerSideProps } from 'next';
-import { useEffect, useState } from 'react';
-import prisma from '~/lib/prisma';
-import { Product } from './interfaces';
+// import { GetServerSideProps } from 'next';
+// import { useEffect, useState } from 'react';
+// import prisma from '~/lib/prisma';
+// import { Product } from './interfaces';
 
-export async function getServerSideProps({ context }) {
-  const products = await prisma.product.findMany();
-  return {
-    props: {
-      products,
-    },
-  };
-}
+// export async function getServerSideProps({ context }) {
+//   const products = await prisma.product.findMany();
+//   return {
+//     props: {
+//       products,
+//     },
+//   };
+// }
 
-export default function Home({ products }) {
-  console.log("Here's the data from getServerSideProps");
-  console.log(products);
+// export default function Home({ products }) {
+//   console.log("Here's the data from getServerSideProps");
+//   console.log(products);
 
-  // const [data, setData] = useState<Product[]>(products);
-  // useEffect(() => {
-  //   async function retrieve() {
-  //     const products = await (await fetch('/api/plants')).json();
-  //     setData([...products]);
-  //   }
-  //   retrieve();
-  // }, []);
+//   // const [data, setData] = useState<Product[]>(products);
+//   // useEffect(() => {
+//   //   async function retrieve() {
+//   //     const products = await (await fetch('/api/plants')).json();
+//   //     setData([...products]);
+//   //   }
+//   //   retrieve();
+//   // }, []);
 
-  // useEffect(() => {
-  //   console.log(data);
-  // }, [data]);
+//   // useEffect(() => {
+//   //   console.log(data);
+//   // }, [data]);
 
+//   return (
+//     <div className="bg-[#283044] w-full h-full min-h-full flex justify-center items-center">
+//       <span className="text-2xl font-bold text-white">
+//         Home of <span className="text-green-600">Monsteras</span> ❤️ 🌱
+//       </span>
+//       <div className="flex flex-col justify-center items-center">
+//         {products ? (
+//           products?.map(({ name }) => {
+//             return (
+//               <span key={name} className="text-white">
+//                 {name}
+//               </span>
+//             );
+//           })
+//         ) : (
+//           <span>Loading...</span>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+import { trpc } from '../utils/trpc';
+export default function IndexPage() {
+  const hello = trpc.hello.useQuery({ text: 'client' });
+  const products = trpc.items.useQuery({ text: 'products' });
+  if (!hello.data || !products.data) {
+    return <div>Loading...</div>;
+  }
+  const productData = products?.data;
+  console.log(products.data);
   return (
-    <div className="bg-[#283044] w-full h-full min-h-full flex justify-center items-center">
-      <span className="text-2xl font-bold text-white">
-        Home of <span className="text-green-600">Monsteras</span> ❤️ 🌱
-      </span>
-      <div className="flex flex-col justify-center items-center">
-        {products ? (
-          products?.map(({ name }) => {
-            return (
-              <span key={name} className="text-white">
-                {name}
-              </span>
-            );
-          })
-        ) : (
-          <span>Loading...</span>
-        )}
-      </div>
+    <div>
+      <p>{hello.data.greeting}</p>
+
+      {productData.map((item) => {
+        return (
+          <div key={`item-${item.name}`}>
+            <span>{item?.name}</span>;
+          </div>
+        );
+      })}
     </div>
   );
 }
