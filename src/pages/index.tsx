214f17f41d862bc9c4cc7ff6e813808cec7e5cbd
Reilly <reillyodie@gmail.com>
2022-10-07 23:55:36 -0400

    Updated UI of the main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,19 +60,44 @@ export default function IndexPage() {
     return <div>Loading...</div>;
   }
   const productData = products?.data;
-  console.log(products.data);
   return (
-    <div>
-      <p>{hello.data.greeting}</p>
-
-      {productData.map((item) => {
-        return (
-          <div key={`item-${item.name}`}>
-            <span>{item?.name}</span>;
-            <img className="w-24 h-24" src={item.image} />
+    <div className="flex justify-center items-center bg-black">
+      <div className="text-white">
+        <p>{hello.data.greeting}</p>
+        <div className="flex flex-col justify-center items-start">
+          <span>Add new item</span>
+          <div>
+            <input
+              placeholder="Add item..."
+              className="bg-transparent px-2 py-1 border-2 border-white rounded-lg"
+            />
+            <button className="border-2 mx-4 px-2 py-1 rounded-lg border-white">
+              Add
+            </button>
           </div>
-        );
-      })}
+        </div>
+
+        <div className="flex ">
+          {productData.map((item) => {
+            const formattedPrice = (item.price / 100).toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            });
+            return (
+              <div
+                className="mx-2 flex flex-col justify-center items-center"
+                key={`item-${item.name}`}
+              >
+                <span>{formattedPrice}</span>
+                <img className="w-24 h-24" src={item.image} />
+                <button className="flex px-4 py-1 my-2 border-2 border-stone-600 rounded-lg transition-all hover:bg-[#0000000c]">
+                  Quick Buy
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
