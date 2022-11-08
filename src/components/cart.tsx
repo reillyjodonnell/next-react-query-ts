0e35daf9667d33cfcbf6393fe2847f941bc0d881
Reilly O'Donnell <odonnell.reilly.j@gmail.com>
2022-11-07 23:03:20 -0500

    Updated Cart dropdown. Implemented Nav header

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -19,50 +19,57 @@ export default function Cart({
           {items?.length ?? 0}
         </span>
       </span>
-      <div className="flex flex-col">
-        {showExpanded
-          ? items?.map((item) => {
-              const formattedPrice = (item?.price / 100 ?? 0).toLocaleString(
-                'en-US',
-                {
-                  style: 'currency',
-                  currency: 'USD',
-                }
-              );
-              const { name, image, quantity, id } = item;
-              return (
-                <div
-                  className="mx-2 flex  justify-center items-center"
-                  key={`item-${name}`}
-                >
-                  <div>
+      <div className="flex flex-col relative">
+        {showExpanded ? (
+          <div className="flex flex-col justify-center items-center absolute mt-2 p-4 right-0 top-0 w-80 h-fit bg-[#141414b6] border-[#ffffff7b] border-2">
+            {items.length > 0 ? (
+              items?.map((item) => {
+                const formattedPrice = (item?.price / 100 ?? 0).toLocaleString(
+                  'en-US',
+                  {
+                    style: 'currency',
+                    currency: 'USD',
+                  }
+                );
+                const { name, image, quantity, id } = item;
+                return (
+                  <div
+                    className="mx-2 flex  justify-center items-center"
+                    key={`item-${name}`}
+                  >
                     <img className="w-24 h-24" src={image} />
-                  </div>
-                  <div className="flex flex-col mx-2">
-                    <span>{formattedPrice}</span>
-                  </div>
-                  <span>{quantity}</span>
-                  <div className="flex flex-col justify-center items-center mx-2">
+                    <div className="flex flex-col mx-2">
+                      <span>{formattedPrice}</span>
+                    </div>
+                    <span>{quantity}</span>
+                    <div className="flex flex-col justify-center items-center mx-2">
+                      <span
+                        className="text-3xl"
+                        onClick={() => increaseQuantity({ id })}
+                      >
+                        +
+                      </span>
+                      <span
+                        className="text-3xl "
+                        onClick={() => subtractQuantity({ id })}
+                      >
+                        -
+                      </span>
+                    </div>
                     <span
-                      className="text-3xl"
-                      onClick={() => increaseQuantity({ id })}
+                      className="text-xl "
+                      onClick={() => removeItem({ id })}
                     >
-                      +
-                    </span>
-                    <span
-                      className="text-3xl "
-                      onClick={() => subtractQuantity({ id })}
-                    >
-                      -
+                      ❌
                     </span>
                   </div>
-                  <span className="text-xl " onClick={() => removeItem({ id })}>
-                    ❌
-                  </span>
-                </div>
-              );
-            })
-          : null}
+                );
+              })
+            ) : showExpanded && items.length === 0 ? (
+              <span>Add something to your cart!</span>
+            ) : null}
+          </div>
+        ) : null}
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,10 +30,123 @@ export default function IndexPage() {
     return <div>Loading...</div>;
   }
   return (
-    <div className="flex justify-center items-center bg-black flex-col mt-4">
-      <span className="flex text-2xl text-white">Store</span>
-      <div className="text-white">
-        <div className="flex justify-end">
+    <div>
+      <Header
+        subtractQuantity={subtractQuantity}
+        increaseQuantity={increaseQuantity}
+        removeItem={removeItem}
+        setItems={setItems}
+        items={items}
+      />
+      <div className="flex justify-center items-center bg-black flex-col mt-4">
+        <span className="flex text-2xl text-white">Store</span>
+        <div className="text-white">
+          <div className="flex justify-end"></div>
+          <div className="flex flex-col justify-center items-center">
+            {create?.isLoading ? (
+              <span>Loading</span>
+            ) : (
+              <div className="flex flex-col justify-center items-center">
+                <label className="flex flex-col my-2">
+                  URL
+                  <input
+                    className="text-white border-2 rounded-md border-white bg-transparent"
+                    onChange={(e) =>
+                      setItem({ ...item, url: e?.target?.value })
+                    }
+                  />
+                </label>
+                <label className="flex flex-col my-2">
+                  Description
+                  <input
+                    className="text-white border-2 rounded-md border-white bg-transparent"
+                    onChange={(e) =>
+                      setItem({ ...item, description: e?.target?.value })
+                    }
+                  />
+                </label>
+                <label className="flex flex-col my-2">
+                  Price
+                  <input
+                    className="text-white border-2 rounded-md border-white bg-transparent"
+                    onChange={(e) =>
+                      setItem({ ...item, price: e?.target?.value })
+                    }
+                  />
+                </label>
+                {create?.error ? <span>Uh oh!</span> : null}
+                <button
+                  onClick={(e) => createItem(e)}
+                  className="border-2 mx-4 px-2 py-1 rounded-lg border-white"
+                >
+                  Add Item
+                </button>
+              </div>
+            )}
+          </div>
+          <div className="grid my-12 grid-cols-3 gap-y-8">
+            {productData.map((item) => {
+              const formattedPrice = (item.price / 100).toLocaleString(
+                'en-US',
+                {
+                  style: 'currency',
+                  currency: 'USD',
+                }
+              );
+              return (
+                <div
+                  className="mx-8 flex flex-col justify-center items-center"
+                  key={`item-${item.name}`}
+                >
+                  <img
+                    className="w-60 h-60 rounded-lg cursor-pointer object-cover"
+                    src={item.image}
+                  />
+                  <div className="flex w-full mt-2">
+                    <div className="flex flex-col justify-start">
+                      <span>{formattedPrice}</span>
+                      <span>{item?.description}</span>
+                    </div>
+                    <button
+                      onClick={() => {
+                        addItem({ item });
+                      }}
+                      className="flex ml-auto w-fit h-fit py-[2px] px-2 border-2 border-stone-600 rounded-lg transition-all hover:bg-[#0000000c] hover:border-white"
+                    >
+                      Buy
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function Header({
+  subtractQuantity,
+  increaseQuantity,
+  removeItem,
+  setItems,
+  items,
+}) {
+  return (
+    <header className="w-full h-20 flex text-white justify-start items-center px-20 border-b border-white">
+      <span className="font-bold text-2xl cursor-pointer">Plant Shop</span>
+      <div className="ml-auto flex">
+        <span className="mx-1 cursor-pointer opacity-90 hover:opacity-100 px-6 py-2 hover:bg-[#ffffff14] transition-all rounded-md">
+          Home
+        </span>
+        <span className="mx-1 cursor-pointer opacity-90 hover:opacity-100 px-6 py-2 hover:bg-[#ffffff14] transition-all rounded-md">
+          Products
+        </span>
+        <span className="mx-1 cursor-pointer opacity-90 hover:opacity-100 px-6 py-2 hover:bg-[#ffffff14] transition-all rounded-md">
+          Explore
+        </span>
+        <div className="mx-1 px-2 flex justify-center items-center relative">
           <Cart
             subtractQuantity={subtractQuantity}
             increaseQuantity={increaseQuantity}
@@ -42,80 +155,7 @@ export default function IndexPage() {
             items={items}
           />
         </div>
-        <div className="flex flex-col justify-center items-center">
-          {create?.isLoading ? (
-            <span>Loading</span>
-          ) : (
-            <div className="flex flex-col justify-center items-center">
-              <label className="flex flex-col my-2">
-                URL
-                <input
-                  className="text-white border-2 rounded-md border-white bg-transparent"
-                  onChange={(e) => setItem({ ...item, url: e?.target?.value })}
-                />
-              </label>
-              <label className="flex flex-col my-2">
-                Description
-                <input
-                  className="text-white border-2 rounded-md border-white bg-transparent"
-                  onChange={(e) =>
-                    setItem({ ...item, description: e?.target?.value })
-                  }
-                />
-              </label>
-              <label className="flex flex-col my-2">
-                Price
-                <input
-                  className="text-white border-2 rounded-md border-white bg-transparent"
-                  onChange={(e) =>
-                    setItem({ ...item, price: e?.target?.value })
-                  }
-                />
-              </label>
-              {create?.error ? <span>Uh oh!</span> : null}
-              <button
-                onClick={(e) => createItem(e)}
-                className="border-2 mx-4 px-2 py-1 rounded-lg border-white"
-              >
-                Add Item
-              </button>
-            </div>
-          )}
-        </div>
-        <div className="grid my-12 grid-cols-3 gap-y-8">
-          {productData.map((item) => {
-            const formattedPrice = (item.price / 100).toLocaleString('en-US', {
-              style: 'currency',
-              currency: 'USD',
-            });
-            return (
-              <div
-                className="mx-8 flex flex-col justify-center items-center"
-                key={`item-${item.name}`}
-              >
-                <img
-                  className="w-60 h-60 rounded-lg cursor-pointer"
-                  src={item.image}
-                />
-                <div className="flex w-full mt-2">
-                  <div className="flex flex-col justify-start">
-                    <span>{formattedPrice}</span>
-                    <span>{item?.description}</span>
-                  </div>
-                  <button
-                    onClick={() => {
-                      addItem({ item });
-                    }}
-                    className="flex ml-auto w-fit h-fit py-[2px] px-2 border-2 border-stone-600 rounded-lg transition-all hover:bg-[#0000000c] hover:border-white"
-                  >
-                    Buy
-                  </button>
-                </div>
-              </div>
-            );
-          })}
-        </div>
       </div>
-    </div>
+    </header>
   );
 }
